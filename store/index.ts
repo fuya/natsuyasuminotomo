d39927cca9e238a5e6e8e99c8133a5723f358e5d
Fuya <dev@fuya.info>
2018-07-27 12:22:38 +0900

    set getters

--- store/index.ts
+++ store/index.ts
@@ -12,6 +12,9 @@ const createStore = () =>
     state: {
       articles: []
     },
+    getters: {
+      articles: state => state.articles
+    },
     mutations: {
       ...firebaseMutations
     },
