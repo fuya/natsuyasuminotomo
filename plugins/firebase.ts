ecc6081de337762437a13ce96096222014b6b7ae
Fuya <dev@fuya.info>
2018-07-27 12:22:28 +0900

    change project

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -3,12 +3,12 @@ import "firebase/database";
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyA88P6kZnNgkhGIMxj3R8gycPKOaBzrUOQ",
-    authDomain: "bookmark-7545f.firebaseapp.com",
-    databaseURL: "https://bookmark-7545f.firebaseio.com",
-    projectId: "bookmark-7545f",
-    storageBucket: "bookmark-7545f.appspot.com",
-    messagingSenderId: "54828696522"
+    apiKey: "AIzaSyAPNE3Q5qxS9VEpNM6ZjjQhBcyUkZn0SsI",
+    authDomain: "myscrap-3ae37.firebaseapp.com",
+    databaseURL: "https://myscrap-3ae37.firebaseio.com",
+    projectId: "myscrap-3ae37",
+    storageBucket: "myscrap-3ae37.appspot.com",
+    messagingSenderId: "1017667156795"
   });
 }
 
